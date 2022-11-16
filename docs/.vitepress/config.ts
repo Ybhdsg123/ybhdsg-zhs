b28e193a9ac82dcf8c91df089050423b4d45e2d9
zhs <1654134943@qq.com>
2022-11-16 17:10:03 +0800

    第一次部署

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vitepress";
 
 export default defineConfig({
+  base: "/ybhdsg-zhs",
   // 页面标题
   title: "哑巴湖大水怪",
   titleTemplate: "我的网站",
   themeConfig: {
     // 网站的标题和logo
     logo: "/cat.svg",
-    siteTitle: "哑巴湖大水怪的山水游记", // 这里写 false 就不现实，不写读取上面的 title
+    siteTitle: "哑巴湖大水怪的山水游记", // 这里写 false 就不显示，不写读取上面的 title
     socialLinks: [
       { icon: "github", link: "https://github.com/gumingWu/vitepress-fun" },
     ],
