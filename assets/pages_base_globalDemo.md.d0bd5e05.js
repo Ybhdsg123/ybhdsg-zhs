451c18e255b9f619add1403fcf25ee70c4264d76
Ybhdsg123 <Ybhdsg123@users.noreply.github.com>
2024-08-07 07:54:53 +0000

    deploy: 88f885a2a0fd5b24dd26ec4328621fa5edac962a

--- assets/pages_base_globalDemo.md.d0bd5e05.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as a,c as t,b as o}from"./app.dafbacc7.js";const b=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"pages/base/globalDemo.md"}'),s={name:"pages/base/globalDemo.md"},n=o("p",null,"111",-1),r=[n];function c(l,_,p,d,i,m){return a(),t("div",null,r)}const g=e(s,[["render",c]]);export{b as __pageData,g as default};
